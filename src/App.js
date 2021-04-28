099a63e84debf411dba6763ae434eb61fea0fd75
Manel Akrimi <akrimimanel42@gmail.com>
2021-04-28 13:36:45 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+  <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+    <h1 className="title red">Hi I'am Manel</h1>
     </div>
+    <br />
+    <img src="Manel.jpg" width={320} height={240} />
+
+    <img src="developer.png" width={320} height={240} />
+    <br />
+  <br />
+  <video width={320} height={240} controls>
+    <source src="What Is A Full-Stack Web Developer_.mp4" type="video/mp4" />
+  </video>
+</div>
+
+        
+      </div>
+    
   );
 }
 
